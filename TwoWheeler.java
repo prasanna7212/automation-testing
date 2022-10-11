1b006a695322cb34067362fef918e84612786765
prasanna7212 <81813282+prasanna7212@users.noreply.github.com>
2022-10-10 22:05:40 -0700

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,24 @@
+package Week1.day1;
+
+public class TwoWheeler {
+public static void main(String[]args) {
+	int noofWheels=2;
+	System.out.println(noofWheels);
+	short noofMirrors=2;
+	System.out.println(noofmirrors);
+	long ChaseNumber=455612378989L;
+	System.out.println( ChaseNumber);
+    boolean ispuntured=false;
+    System.out.println(ispuntured );
+	String bikeName="Re";
+	System.out.println(bikeName);
+	double runningKM=95.9456;
+	System.out.println(runningKM);
+
+	
+	
+
+}
+
+}
+
